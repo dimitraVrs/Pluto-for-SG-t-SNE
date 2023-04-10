d6804b59385b5c4166ac28889f4eb4cf32ab76fe
Dimitra Varsami <dimitra_vrs@hotmail.com>
2023-04-10 22:18:50 +0300

    Delete pluto data directory

--- pluto data/matlabScripts/similarityMatrix.m
+++
@@ -1,7 +0,0 @@
-function D = similarityMatrix(X,n)
-    D = squareform(pdist(X));  % all-to-all distance matrix
-
-    D = 1 ./ D;                % inverse distance as similarity
-    for i=1:n; D(i,i) = 0; end % set Inf to zero on the diagonal
-    D = D ./ sum(D,1);         % make column stochastic
-end
